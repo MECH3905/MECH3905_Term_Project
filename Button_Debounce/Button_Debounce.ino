3b4a76606fdb8f351d51f5e6844912ef2ab44f1a
Qfox1 <qn318632@dal.ca>
2026-04-02 20:34:03 -0300

    Update Button_Debounce.ino

--- Button_Debounce/Button_Debounce.ino
+++ Button_Debounce/Button_Debounce.ino
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Button Presses 
 
 // Button Pins
@@ -5,28 +6,42 @@ const int jumpPin = 4;
 const int dashPin = 5;
 const int crouchPin = 6;
 const int jabPin = 7;
+=======
+// MECH3905 Term Project
+// Date: April 6, 2026
+// Group 6 Members: 
+// Eric Adamson (B00962642)
+// Francesco Borrelli (B00964461)
+// Quinn Fox (B01020683)
+// Purpose: Send button and joystick info to MATLAB
+
+// Button Pins
+const int crouchPin = 8; // Digital pin 8 represents the crouch pin
+const int dashPin = 9;   // Digital pin 9 represents the dash pin
+const int jabPin = 10;   // Digital pin 10 represents the jab pin 
+const int jumpPin = 11;  // Digital pin 11 represents the jump pin
+>>>>>>> Stashed changes
 
 // Joystick Pins
-const int xPin = A0;
-const int yPin = A1;
+const int xPin = A0; // Analog pin 0 represents the joystick x-axis pin
+const int yPin = A1; // Analog pin 1 represents the joystick y-axis pin 
 
-// Debounce
-const unsigned long debounceDelay = 50;
+const unsigned long debounceDelay = 50; // Debounce delay set to 50 miliseconds 
 
 // Button outputs 
-bool jumpBtn = 1;
-bool dashBtn = 1;
-bool crouchBtn = 1;
-bool jabBtn = 1;
+bool jumpBtn = 1;   // Jump button state set to 1 (not pressed)
+bool dashBtn = 1;   // Dash button state set to 1 (not pressed)
+bool crouchBtn = 1; // Crouch button state set to 1 (not pressed)
+bool jabBtn = 1;    // Jab button state set to 1 (not pressed)
 
 // States
-int lastJumpReading = HIGH;
-int lastDashReading = HIGH;
-int lastJabReading  = HIGH;
+int lastJumpReading = HIGH; // Last reading of jump button set to high (not pressed, used for debounce)
+int lastDashReading = HIGH; // Last reading of dash button set to high (not pressed, used for debounce)
+int lastJabReading  = HIGH; // Last reading of jab button set to high (not pressed, used for debounce)
 
-int stableJumpState = HIGH;
-int stableDashState = HIGH;
-int stableJabState  = HIGH;
+int stableJumpState = HIGH; // Stable jump debounce state set to HIGH
+int stableDashState = HIGH; // Stable dash debounce state set to HIGH
+int stableJabState  = HIGH; // Stable jab debounce state set to HIGH
 
 unsigned long lastDebounceTimeJump = 0;
 unsigned long lastDebounceTimeDash = 0;
